457832af4ddd9ed06a2557d9862bfbbd3b88a9e5
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-16 20:14:37 -0500

    Removed route console.log and imports

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,7 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
-import { Page } from "../styles/templates";
-import { Header } from "./Header";
+import { Page } from '../styles/templates';
+import { Header } from './Header';
 
 export const Layout = (props: { children: JSX.Element }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    console.log(router);
-  }, [router]);
-
   return (
     <Page>
       <Header />
