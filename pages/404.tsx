d6f448d91de734aa18f124c05d7d3b21b5aec017
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-16 20:14:56 -0500

    Created 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,24 @@
-import Image from 'next/image';
+import Head from 'next/head';
+import { Button } from 'components/Button';
 
-const NotFound = () => {};
+import styles from 'styles/notFound.module.css';
+
+const { errorPage, error404, errorMessage } = styles;
+
+const NotFound = () => {
+  return (
+    <>
+      <Head>
+        <title> 404 Not Found | Amaury Vasquez </title>
+      </Head>
+      <div className={`${errorPage}`}>
+        <span className={error404}>404</span>
+        <br />
+        <span className={`${errorMessage}`}>{'Not found'}</span>
+        <Button link="/" text="Return home" />
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
