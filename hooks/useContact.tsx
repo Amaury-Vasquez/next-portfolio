fff5eaef54ebe5a2b9e927e4f94761da1fac597c
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-11-12 22:56:09 -0600

    Change render mode

--- hooks/useContact.tsx
+++ hooks/useContact.tsx
@@ -3,22 +3,22 @@ import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 export const useContact = () => {
   const icons = [
     {
-      icon: <FaInstagram />,
+      icon: FaInstagram,
       link: 'https://www.instagram.com/vasquezamaury/',
       color: '#f60c56',
     },
     {
-      icon: <FaTwitter />,
+      icon: FaTwitter,
       link: 'https://twitter.com/AmauryVasquez11',
       color: '#1d9bf0',
     },
     {
-      icon: <FaGithub />,
+      icon: FaGithub,
       link: 'https://github.com/Amaury-Vasquez/',
       color: 'white',
     },
     {
-      icon: <FaLinkedin />,
+      icon: FaLinkedin,
       link: 'https://www.linkedin.com/in/amaury-vasquez-9a1b3a240/',
       color: '#0a66c2',
     },
